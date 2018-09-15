075ab1d0d70d086f395d9feaf2480e6add043c48
albericoj3 <36242632+albericoj3@users.noreply.github.com>
2018-09-15 13:03:45 -0400

    Add files via upload

---
+++ Simple Blink/Lab1_Embedded_Simple_Blink_P401R__MSP432/main.c
@@ -0,0 +1,23 @@
+#include <msp432.h>
+
+
+/**
+ * Button Blink G2
+ * Author: Jordan Alberico
+ * Created: 9/12/18
+ * Last Edited: 9/12/18
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1SEL0 &= ~BIT0; //configure Port 1 for pin 0
+	P1DIR |= BIT0; //sets port 1.0 to 0
+
+	while(1)
+	{
+	    P1OUT ^= BIT0; //toggles LED
+	    __delay_cycles(1000000); //delays LED by ~1 sec
+	}
+	
+	return 0;
+}
